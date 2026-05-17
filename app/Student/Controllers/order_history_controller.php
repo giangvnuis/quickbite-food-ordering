31b11436cee63392dce0c59d3c5218b37d4026ab
VuThiKhanhHuyen23070560 <23070560@vnu.edu.vn>
2026-05-17 23:36:39 +0700

    Add order history controller for student orders

---
+++ app/Student/Controllers/order_history_controller.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../student_context.php';
+require_once __DIR__ . '/../Services/order_history_service.php';
+$ctx = student_ctx();
+$conn = $ctx['conn'];
+$user = $ctx['user'];
+extract(student_order_history_page_data($conn, (int) $user['id'], $_GET), EXTR_SKIP);
+require_once __DIR__ . '/../Views/order-history.php';
